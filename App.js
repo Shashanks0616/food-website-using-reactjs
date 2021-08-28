7f171d9244957a0b93a4facf138a173f6a6917d3
Shashank Singh <80109899+Shashanks0616@users.noreply.github.com>
2021-08-28 20:48:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Navbar from './component/Navbar';
+import Content from './component/Content';
+import Contact from './component/Contact';
+import Pricing from './component/Pricing';
+// import Fttr from './component/Fttr';
+// import Footer from'./component/Footer';
+
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Content/>
+    <Contact/>
+    <Pricing/>
+    </>
+  );
+}
+
+export default App;
---
+++ Contact.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+export default function Contact() {
+    return (
+      <div className="contact">
+        <section class="text--600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap w-full mb-20">
+      <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
+        <div class="h-1 w-20 bg-indigo-500 rounded"></div>
+      </div>
+      <p class="lg:w-1/2 w-full leading-relaxed text-black">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+    </div>
+    <div class="flex flex-wrap -m-4">
+      <div class="xl:w-1/4 md:w-1/2 p-4">
+        <div class="bg-gray-100 p-6 rounded-lg">
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://im.whatshot.in/img/2019/Jun/shutterstock-1153818823-cropped-1561026121.jpg" alt="content"/>
+          <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">Food</h3>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">North Indian</h2>
+          <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        </div>
+      </div>
+      <div class="xl:w-1/4 md:w-1/2 p-4">
+        <div class="bg-gray-100 p-6 rounded-lg">
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://www.sodhatravel.com/hs-fs/hubfs/North%20India%20Food%20Collage.jpg?width=5120&name=North%20India%20Food%20Collage.jpg" alt="content"/>
+          <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">Food</h3>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">North Indian</h2>
+          <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        </div>
+      </div>
+      <div class="xl:w-1/4 md:w-1/2 p-4">
+        <div class="bg-gray-100 p-6 rounded-lg">
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://wallsdesk.com/wp-content/uploads/2017/01/Fast-Food-Photos.jpg" alt="content"/>
+          <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">Food</h3>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Fast Food</h2>
+          <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        </div>
+      </div>
+      <div class="xl:w-1/4 md:w-1/2 p-4">
+        <div class="bg-gray-100 p-6 rounded-lg">
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://b70dfa05131d25294e9b-54e438e9b55e4e5a6a4efdac2ed74007.ssl.cf1.rackcdn.com/HotColdDrinks_15.jpg" alt="content"/>
+          <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font ">Food</h3>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Beverages</h2>
+          <p class="leading-relaxed text-base">We have Beverages is now a preferred choice in the retail market as well in the corporate sector for quality beverage premixes. Large multinationals, government organisations and popular airlines also trust Plus Beverages when it comes to offering taste + quality to their consumers. Plus also offers clients vending machines for beverages,cold drinks, etc.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</div>
+        )
+}
